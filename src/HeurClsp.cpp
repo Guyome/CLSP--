442a00f261cf09decffbe65919452a59fffdb515
Guillaume Lanquepin <guyomel@gmail.com>
2009-05-29 11:54:48 +0200

    import data from double array in place of blitz array

--- src/HeurClsp.cpp
+++ src/HeurClsp.cpp
@@ -1,36 +1,35 @@
-#include <stdOUTPUT.h>
+#include <stdio.h>
 #include <blitz/array.h>
 #include "HeurClsp.hpp"
 
 using namespace blitz;
-using namespace HeurClsp;
 
-void HeurClsp(Array<double,2> _alpha, Array<double,2> _beta, Array<double,2> _prod, Array<double,2> _stor,
-        Array<double,2> consumptOUTPUTn, Array<double,2> _setup, Array<double,1> _constraint, int _period,
+HeurClsp::HeurClsp(double* _alpha, double* _beta, double* _prod, double* _stor,
+        double* consumption, double* _setup, double* _constraint, int _period,
         int _product, int _verbose)
 {
     period = _period;
     product = _product;
-    alpha = _alpha;
-    beta = _beta;
-    prod = _prod;
-    stor = _stor;
-    cons = consumptOUTPUTn;
-    setup = _setup;
-    constraint =_constraint;
+    alpha = new Array<double,2>(_alpha, shape(product,period), neverDeleteData);
+    beta = new Array<double,2>(_beta, shape(product,period), neverDeleteData);
+    prod = new Array<double,2>(_prod, shape(product,period), neverDeleteData);
+    stor = new Array<double,2>(_stor, shape(product,period), neverDeleteData);
+    cons = new Array<double,2>(consumption, shape(product,period), neverDeleteData);
+    setup = new Array<double,2>(_setup, shape(product,period), neverDeleteData);
+    constraint = new Array<double,1>(_constraint, shape(period), neverDeleteData);
     coef = new Array<double,1>((product+1)*period) ; //KKT initiate as null
     
     verbose = _verbose;
 }
         
-Array<int,2> thomas(Array<double,2> results)
+Array<int,2> HeurClsp::thomas(Array<double,2> results)
 {
-    Array<double,1> c(hor);//cost function
-    Array<double,1> f(hor+1);//objective function
-    Array<double,2> price(hor,hor);
-    Array<double,2> demand(hor,hor);
-    Array<int,2> ind(hor);//production structure
-    float sumc;
+    Array<double,1> c(period);//cost function
+    Array<double,1> f(period+1);//productective function
+    Array<double,2> price(period,period);
+    Array<double,2> demand(period,period);
+    Array<int,2> ind(period);//production structure
+    double sumc;
     int t;//current time period
 
 ////OUTPUT
@@ -41,34 +40,36 @@ Array<int,2> thomas(Array<double,2> results)
     }
 ////OUTPUT
 
-    for(int j = 0; j < obj;  j++)
+    for(int j = 0; j < product;  j++)
     {
         //initiate price,demand since there is no storage
         t = 0;
-        price(t,t) = (alpha(j,t) + (prod(j,t) + cons(j,t)*coef(t))* beta(j,t)) / (2 * beta(j,t) );
-        demand(t,t) = alpha(j,t) - beta(j,t) * price(t,t);
-        c(t) = demand(t,t)*( price(t,t) - prod(j,t))-setup(j,t);
+        price(t,t) = ((*alpha)(j,t) + ((*prod)(j,t) + (*cons)(j,t)*(*coef)(t))
+            * (*beta)(j,t)) / (2 * (*beta)(j,t) );
+        demand(t,t) = (*alpha)(j,t) - (*beta)(j,t) * price(t,t);
+        c(t) = demand(t,t)*( price(t,t) - (*prod)(j,t))-(*setup)(j,t);
         f(t) = 0;
         f(t+1) = -c(t);
         //result for the first period
         ind(j,0) = 0;
         results(j,t)=price(t,(int)ind(j,t));
-        for (t = 1; t < hor;  t++)
+        for (t = 1; t < period;  t++)
         {
             for (int t0 = 0; t0 <= t; t0++)
             {
                 //compute price
-                price(t,t0) = (alpha(j,t) + (prod(j,t0) + sum(stor(j,Range(t0,t-1)))
-                    + cons(j,t0)*coef(t0))* beta(j,t)) / (2 * beta(j,t));
+                price(t,t0) = ((*alpha)(j,t) + ((*prod)(j,t0) + sum((*stor)(j,Range(t0,t-1)))
+                    + (*cons)(j,t0)*(*coef)(t0))* (*beta)(j,t)) / (2 * (*beta)(j,t));
                 //compute demand
-                demand(t,t0) = alpha(j,t) - beta(j,t) * price(t,t0);
+                demand(t,t0) = (*alpha)(j,t) - (*beta)(j,t) * price(t,t0);
                 //compute cost
                 sumc = 0;
                 for (int i = t0; i <= t; i++)
                 {
-                    sumc += (prod(j,t0) + cons(j,t0)*coef(t0) + sum(stor(j,Range(t0,i-1))) - price(i,t0))*demand(i,t0);
+                    sumc += ((*prod)(j,t0) + (*cons)(j,t0)*(*coef)(t0)
+                        + sum((*stor)(j,Range(t0,i-1))) - price(i,t0))*demand(i,t0);
                 }
-                c(t0) = sumc + setup(j,t0);
+                c(t0) = sumc + (*setup)(j,t0);
             }
             //find minimal criterium
             f(t+1) = min(c(Range(0,t)) + f(Range(0,t)));
@@ -80,7 +81,7 @@ Array<int,2> thomas(Array<double,2> results)
     ////OUTPUT
         if (verbose >2)
         {
-            for (t = 0; t < hor; t++)
+            for (t = 0; t < period; t++)
             {
                 printf("%d\t%d\t%f\t%d\n",j+1,t+1,-f(t+1),(int)ind(j,t)+1);
             }
@@ -98,7 +99,5 @@ Array<int,2> thomas(Array<double,2> results)
 
     return ind;
 }
-
-
 
 
--- src/HeurClsp.hpp
+++ src/HeurClsp.hpp
@@ -7,27 +7,27 @@ using namespace blitz;
 class HeurClsp
 {
 private:
-    int verbose//verbosity parameter
+    int verbose;//verbosity parameter
     int period;//number of period
     int product;//number of product
     //demand function is linear
-    Array<double,2> alpha;//slope
-    Array<double,2> beta;//intercept
-    Array<double,2> prod;//production cost
-    Array<double,2> stor;//holding cost
-    Array<double,2> cons;//consumption of ressouce 
-    Array<double,2> setup;//setup structure
-    Array<double,1> constraint;//prodcution constraint
-    Array<double,1> coef;//Khun Thucker coeficient
+    Array<double,2>* alpha;//slope
+    Array<double,2>* beta;//intercept
+    Array<double,2>* prod;//production cost
+    Array<double,2>* stor;//holding cost
+    Array<double,2>* cons;//consumption of ressouce 
+    Array<double,2>* setup;//setup structure
+    Array<double,1>* constraint;//prodcution constraint
+    Array<double,1>* coef;//Khun Thucker coeficient
 
 protected:
     //dynamic programming solver based on Thomas's paper
     Array<int,2> thomas(Array<double,2> results);
-    
+
 public:
     //default constructor
-    HeurClsp(Array<double,2> alpha, Array<double,2> beta, Array<double,2> prod, Array<double,2> stor,
-        Array<double,2> consumption, Array<double,2> setup, Array<double,1> constraint, int period,
-        int product);
-}
+    HeurClsp(double* alpha, double* beta, double* prod, double* stor,
+        double* consumption, double* setup, double* constraint, int period,
+        int product, int verbose);
+};
 #endif

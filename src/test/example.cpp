ca623913802f781448cdc750879f476cb1fea2eb
Guillaume Lanquepin <guyomel@gmail.com>
2009-05-29 11:56:23 +0200

    add test case and makefile

---
+++ src/test/example.cpp
@@ -0,0 +1,20 @@
+#include "../HeurClsp.hpp"
+#include <stdio.h>
+
+int main(int argv, char* argc[])
+{
+    int verb = 2;
+    int J = 1;
+    int T = 4;
+    double a[] = {100.,100.,100.,100.};//slope demand function
+    double b[] = {1.,1.,1.,1.};//intercept demand function
+    double v[] = {20.,20.,20.,20.};//production cost
+    double h[] = {2.,2.,2.,2.};//storage cost
+    double r[] = {18, 20., 32., 12.};//constraint
+    double c[] = {1.,1.,1.,1.};//product consumption
+    double s[] = {1,1,1,1};//setup
+    
+    HeurClsp* example = new HeurClsp(a,b,v,h,c,s,r,T,J,verb);
+
+    printf("OK\n");
+}

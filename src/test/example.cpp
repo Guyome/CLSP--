bfe3cbf9164b2a53640a73ffbd538827c4c32b02
Jessica Chesnais <jess@jess-desktop.(none)>
2009-06-01 15:28:28 +0200

    modification for coerrence with HeurClsp modifications

--- src/test/example.cpp
+++ src/test/example.cpp
@@ -6,6 +6,9 @@ int main(int argv, char* argc[])
     int verb = 2;
     int J = 1;
     int T = 4;
+    int cycle = 100;
+    double param = 0.3;
+    double eps = 1.;
     double a[] = {100.,100.,100.,100.};//slope demand function
     double b[] = {1.,1.,1.,1.};//intercept demand function
     double v[] = {20.,20.,20.,20.};//production cost
@@ -14,7 +17,7 @@ int main(int argv, char* argc[])
     double c[] = {1.,1.,1.,1.};//product consumption
     double s[] = {1,1,1,1};//setup
     
-    HeurClsp* example = new HeurClsp(a,b,v,h,c,s,r,T,J,verb);
+    HeurClsp* example = new HeurClsp(a,b,v,h,c,s,r,T,J,verb,cycle,eps,param);
 
     printf("OK\n");
 }

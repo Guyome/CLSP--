70d75beaee02c1dabd872199cae69485c9890677
Guillaume Lanquepin <guyomel@gmail.com>
2009-06-22 15:51:25 +0200

    modify to import data from test.csv add plotting for optimum variable

--- src/Python/test/example.py
+++ src/Python/test/example.py
@@ -18,16 +18,26 @@ address = 'test.csv'
 
 #uncomment following comment to
 #import data from csv file 
-#"""
+"""
 slope, intercept, prodcost,\
 holdcost, setupcost, consumption,\
-constraint  = tl.generateparam (nb_obj,time_hor)
+constraint = tl.generateparam (nb_obj,time_hor)
 
 tl.exportdata(address,slope, intercept, prodcost, holdcost,
     setupcost, consumption, constraint, nb_obj, time_hor)
-#"""
-#tl.importdata(address)
+"""
+
+time_hor, nb_obj, slope, intercept,\
+setupcost, holdcost, prodcost,\
+consumption, constraint = tl.importdata(address)
 
 test = sl.heurclsp(slope, intercept, prodcost,
     holdcost, consumption, setupcost, constraint, time_hor, nb_obj, verbose, cycle, eps, param)
 test.heursolver()
+
+print "Price:","\n\t".join([str(test.price[j]) for j in range(nb_obj)])
+print "Production:","\n\t".join([str(test.prod[j]) for j in range(nb_obj)])
+print "Holding:","\n\t".join([str(test.hold[j]) for j in range(nb_obj)])
+print "Setup:","\n\t".join([str(test.setup[j]) for j in range(nb_obj)])
+print "KKT coef:",str(test.kkt)
+

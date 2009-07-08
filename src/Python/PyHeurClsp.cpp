4e23878bee4381dfd15ead61b5a8824eaab9cc05
Guillaume Lanquepin <guyome@ubuntu.local>
2009-07-08 16:13:19 +0200

    add docstring modify to rename function in python modify example.py to take in count of python librairy modifications

--- src/Python/PyHeurClsp.cpp
+++ src/Python/PyHeurClsp.cpp
@@ -9,24 +9,33 @@ void (HeurClsp::*thomasx3)(list) = &HeurClsp::thomas;
 
 BOOST_PYTHON_MODULE(heurclsp)
 {
-    class_<HeurClsp>("heurclsp",init<list, list, list, list, list, list, list, int, int, int, int, float, float>())
+    scope().attr("__doc__") =
+        "This library implements some algorithms\n"
+        " for lot-size, capacited lot-size, profit maximizing lot-size\n"
+        " and profit maximizing  capacited lot-size problem for continus\n"
+        " and discret prices.\n\n"
+        " It uses Ipopt solver from COINOR project.\n";
+    class_<HeurClsp>("pclsp",init<list, list, list, list, list, list, list, int, int, int, int, float, float>())
         .add_property("verbose",&HeurClsp::getVerbosity,&HeurClsp::setVerbosity)
-        .add_property("nbcycle",&HeurClsp::getNbCycle,&HeurClsp::setNbCycle)
-        .add_property("mindiff",&HeurClsp::getStopDiff,&HeurClsp::setStopDiff)
-        .add_property("smoothparam",&HeurClsp::getSmooth,&HeurClsp::setSmooth)
+        .add_property("nb_cycle",&HeurClsp::getNbCycle,&HeurClsp::setNbCycle)
+        .add_property("min_diff",&HeurClsp::getStopDiff,&HeurClsp::setStopDiff)
+        .add_property("smooth_param",&HeurClsp::getSmooth,&HeurClsp::setSmooth)
         .def_readonly("price",&HeurClsp::getPrice)
         .def_readonly("hold",&HeurClsp::getHold)
         .def_readonly("prod",&HeurClsp::getProd)
         .def_readonly("setup",&HeurClsp::getSetup)
         .def_readonly("kkt",&HeurClsp::getCoef)
         .def_readonly("gap",&HeurClsp::getGAP)
-        .def("thomas", thomasx1)
-        .def("thomas", thomasx2)
-        .def("thomas", thomasx3)
-        .def("heursolver", &HeurClsp::heursolver)
-        .def("objective", &HeurClsp::objective)
-        .def("useheur", &HeurClsp::setHeur)
-        .def("isWW",&HeurClsp::isWW)
-        .def("isDiscret",&HeurClsp::isDiscret)
+        .def("Objective", &HeurClsp::objective)
+        .def("noQP", &HeurClsp::setHeur, "Use an heuristic in spite of QP solver in PCLSPSolvHeur()\n")
+        .def("isLSP",&HeurClsp::isWW, "Returns True if you are in lot-size case\n")
+        .def("isDiscret",&HeurClsp::isDiscret, "Returns True if you are using discret price")
+        .def("DynProgSolv", thomasx1, "Solser based on dynamic programming\n"
+            "Solve profit lot-size problem\nSee. J. Thomas(1970)\n")
+        .def("DynProgSolv", thomasx2, "Solser based on dynamic programming\n"
+            "Solve lot-size problem\nSee Wagner and Within(1958)\n")
+        .def("DynProgSolv", thomasx3, "Solser based on dynamic programming\n"
+            "Modificatied Thomas's algorithm for discrets prices'\n")
+        .def("PCLSPSolvHeur", &HeurClsp::heursolver, "Heuristic for PCLSP developped by K.K. Haugen and al.")
     ;
 }
--- src/Python/test/example.py
+++ src/Python/test/example.py
@@ -7,6 +7,8 @@
 import heurclsp as sl
 import tools as tl
 
+#print information about heurclsp lib
+print sl.__doc__
 
 time_hor = 2
 nb_obj = 2 
@@ -17,26 +19,38 @@ verbose = 3
 address = 'test.csv'
 
 #uncomment following comment to
-#import data from csv file 
+#generate random paramter
 """
 slope, intercept, prodcost,\
 holdcost, setupcost, consumption,\
 constraint = tl.generateparam (nb_obj,time_hor)
-
+"""
+#uncomment following comment to
+#generate random paramter
+"""
 tl.exportdata(address,slope, intercept, prodcost, holdcost,
     setupcost, consumption, constraint, nb_obj, time_hor)
 """
 
+#import data from csv file 
 time_hor, nb_obj, slope, intercept,\
 setupcost, holdcost, prodcost,\
 consumption, constraint = tl.importdata(address)
 
-test = sl.heurclsp(slope, intercept, prodcost,
+print "Initiate\n"
+test = sl.pclsp(slope, intercept, prodcost,
     holdcost, consumption, setupcost, constraint, time_hor, nb_obj, verbose, cycle, eps, param)
-#test.useheur()
-test.heursolver()
-print test.gap
 
+#print info and mains functions
+print test.PCLSPSolvHeur.__doc__
+print test.DynProgSolv.__doc__
+print test.noQP.__doc__
+
+print "Solve PCSLP\n"
+#test.noQP() #uncomment to use heuristic 
+test.PCLSPSolvHeur()
+
+#uncomment to print optimals variables
 """
 print "Price:","\n\t".join([str(test.price[j]) for j in range(nb_obj)])
 print "Production:","\n\t".join([str(test.prod[j]) for j in range(nb_obj)])
